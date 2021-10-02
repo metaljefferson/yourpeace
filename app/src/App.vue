9148ab5936f59bcf534718cb6524dd8bf63fdcb3
metaljefferson <metaljefferson11@hotmail.com>
2021-10-02 18:18:02 -0300

    Primeiro commit

---
+++ app/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <Header msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+import Header from './components/Header.vue'
+import './assets/styles/fonts.scss'
+export default {
+  name: 'App',
+  components: {
+    Header
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ app/src/components/Header.vue
@@ -0,0 +1,19 @@
+<template>
+  <header>
+   Insight to nourish your spiritual path
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+  components: {
+  }
+}
+</script>
+
+<style lang="scss">
+header {
+  font-family: Winter Insight;
+}
+</style>
